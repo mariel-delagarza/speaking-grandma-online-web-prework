b9234809a811ba4d5968709a404f0e3fa03fca3f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-10-26 10:45:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,6 @@
 def speak_to_grandma(phrase) 
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN!"
   else 
     return "NO, NOT SINCE 1938!"
   end
